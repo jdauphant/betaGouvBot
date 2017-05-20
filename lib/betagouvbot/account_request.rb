a6774da88563da0322d445e1a41d1e783709750f
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2017-05-20 22:08:36 +0200

    Turn AccountRequest into instantiable class

--- lib/betagouvbot/account_request.rb
+++ lib/betagouvbot/account_request.rb
@@ -2,65 +2,61 @@
 # frozen_string_literal: true
 
 module BetaGouvBot
-  module AccountRequest
-    module_function
-
+  class AccountRequest
     InvalidNameError   = Class.new(StandardError)
     InvalidEmailError  = Class.new(StandardError)
 
-    class << self
-      def call(authors, fullname, email, password)
-        validate_fullname!(fullname)
-        validate_email!(email)
+    def call(authors, fullname, email, password)
+      validate_fullname!(fullname)
+      validate_email!(email)
 
-        authors
-          .select { |author| author[:id] == fullname }
-          .flat_map { |author| request_account(author, email, password) }
-      end
+      authors
+        .select { |author| author[:id] == fullname }
+        .flat_map { |author| request_account(author, email, password) }
+    end
 
-      private
+    private
 
-      def validate_fullname!(fullname)
-        fullname &&
-          fullname == fullname[/\A[a-z\.\-.]+\z/] ||
-          raise(InvalidNameError, 'Le format du nom doit être prenom.nom')
-      end
+    def validate_fullname!(fullname)
+      fullname &&
+        fullname == fullname[/\A[a-z\.\-.]+\z/] ||
+        raise(InvalidNameError, 'Le format du nom doit être prenom.nom')
+    end
 
-      def validate_email!(email)
-        email &&
-          email =~ /\A\*?([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i ||
-          raise(InvalidEmailError, 'Email invalide, typo ?')
-      end
+    def validate_email!(email)
+      email &&
+        email =~ /\A\*?([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i ||
+        raise(InvalidEmailError, 'Email invalide, typo ?')
+    end
 
-      def request_account(author, email, password)
-        create_account(author, password) +
-          create_redirection(author, email) +
-          create_notification(author, email)
-      end
+    def request_account(author, email, password)
+      create_account(author, password) +
+        create_redirection(author, email) +
+        create_notification(author, email)
+    end
 
-      def create_account(member, password)
-        [AccountAction.new(member[:id], password)]
-      end
+    def create_account(member, password)
+      [AccountAction.new(member[:id], password)]
+    end
 
-      def create_redirection(member, target)
-        redirect?(target) ? [RedirectAction.new(member[:id], target)] : []
-      end
+    def create_redirection(member, target)
+      redirect?(target) ? [RedirectAction.new(member[:id], target)] : []
+    end
 
-      def create_notification(member, personal_address)
-        context = { 'author' => member }
-        context['redirect'] = personal_address if redirect?(personal_address)
-        personal_address = personal_address[1..-1] unless redirect?(personal_address)
-        mail = Mail.from_file('data/mail_compte.md', [personal_address])
-        [MailAction.new(client, mail.format(context))]
-      end
+    def create_notification(member, personal_address)
+      context = { 'author' => member }
+      context['redirect'] = personal_address if redirect?(personal_address)
+      personal_address = personal_address[1..-1] unless redirect?(personal_address)
+      mail = Mail.from_file('data/mail_compte.md', [personal_address])
+      [MailAction.new(client, mail.format(context))]
+    end
 
-      def redirect?(personal_address)
-        !personal_address.start_with?('*')
-      end
+    def redirect?(personal_address)
+      !personal_address.start_with?('*')
+    end
 
-      def client
-        Mailer.client
-      end
+    def client
+      Mailer.client
     end
   end
 end
